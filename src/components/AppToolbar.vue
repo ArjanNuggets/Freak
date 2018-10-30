a4862490e2f2bdc05a153a834eb4eb964d37b6c7
ArjanNuggets <arianderviishaj@gmail.com>
2018-10-31 00:03:17 +0100

    my man

---
+++ src/components/AppToolbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-toolbar color='grey darken-4' dark>
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title>Freak</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <div class="hidden-sm-and-down">
+
+      <v-btn flat>About</v-btn>
+
+      <v-btn flat>Contact</v-btn>
+
+      <router-link to='/login'>
+        <v-btn flat>Login</v-btn>
+      </router-link>
+
+    </div>
+  </v-toolbar>
+</template>
